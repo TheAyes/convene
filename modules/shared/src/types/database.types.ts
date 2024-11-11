b8bb7b7007a195271549090e56946c3c1b6399a3
ayes <github.com.faculty419@passmail.net>
2024-11-11 21:35:10 +0100

    Enable manual triggering for update-types workflow
    Added `workflow_dispatch` to the `update-types.yml` GitHub Actions workflow file, allowing for manual triggering of the workflow. This change provides more flexibility in running the updates whenever needed without waiting for the scheduled cron job.

---
+++ modules/shared/src/types/database.types.ts
@@ -0,0 +1,234 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      friendships: {
+        Row: {
+          accepted_at: string | null
+          created_at: string
+          from_user: string
+          to_user: string
+        }
+        Insert: {
+          accepted_at?: string | null
+          created_at?: string
+          from_user?: string
+          to_user: string
+        }
+        Update: {
+          accepted_at?: string | null
+          created_at?: string
+          from_user?: string
+          to_user?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "friendships_from_user_fkey"
+            columns: ["from_user"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["account_name"]
+          },
+          {
+            foreignKeyName: "friendships_to_user_fkey"
+            columns: ["to_user"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["account_name"]
+          },
+        ]
+      }
+      messages: {
+        Row: {
+          content: string
+          deleted_at: string | null
+          edit_history: Json[] | null
+          from_user: string
+          message_id: string
+          reactions: Json[] | null
+          sent_at: string
+          to_user: string | null
+        }
+        Insert: {
+          content?: string
+          deleted_at?: string | null
+          edit_history?: Json[] | null
+          from_user: string
+          message_id?: string
+          reactions?: Json[] | null
+          sent_at?: string
+          to_user?: string | null
+        }
+        Update: {
+          content?: string
+          deleted_at?: string | null
+          edit_history?: Json[] | null
+          from_user?: string
+          message_id?: string
+          reactions?: Json[] | null
+          sent_at?: string
+          to_user?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "messages_from_user_fkey"
+            columns: ["from_user"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["account_name"]
+          },
+          {
+            foreignKeyName: "messages_to_user_fkey"
+            columns: ["to_user"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["account_name"]
+          },
+        ]
+      }
+      profiles: {
+        Row: {
+          account_name: string
+          avatar_url: string | null
+          display_name: string | null
+          online_status: Database["public"]["Enums"]["online_states"]
+          updated_at: string
+          user_id: string
+        }
+        Insert: {
+          account_name?: string
+          avatar_url?: string | null
+          display_name?: string | null
+          online_status?: Database["public"]["Enums"]["online_states"]
+          updated_at?: string
+          user_id?: string
+        }
+        Update: {
+          account_name?: string
+          avatar_url?: string | null
+          display_name?: string | null
+          online_status?: Database["public"]["Enums"]["online_states"]
+          updated_at?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      online_states: "online" | "idle" | "busy" | "offline" | "disconnected"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
